adb808063c305b2d8b1b1277b38df3b496a1b5bb
Peter <PeterRong96@gmail.com>
2020-02-18 23:05:21 -0800

    using println to allow panic information
    Signed-off-by: Peter <PeterRong96@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 #![feature(format_args_nl)]
 #![no_main]
 #![no_std]
+#![feature(panic_info_message)]
 
 #![doc(html_logo_url = "https://git.io/JeGIp")]
 
@@ -22,5 +23,5 @@ mod interface;
 /// - Only a single core must be active and running this function.
 unsafe fn kernel_init() -> ! {
     println!("Hello from Rust!");
-    panic!();
+    panic!("Kernel panicked");
 }
--- src/panic.rs
+++ src/panic.rs
@@ -1,6 +1,12 @@
 use core::panic::PanicInfo;
+use crate::println;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(args) = info.message() {
+        println!("\nKernel panic: {}", args);
+    } else {
+        println!("\nKernel panic!");
+    }
     crate::arch::wait_forever()
 }
