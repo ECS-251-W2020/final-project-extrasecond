0555378a469ede944ead53a7e4f1c018a3d7873b
Peter <PeterRong96@gmail.com>
2020-02-21 15:11:24 -0800

    delete old lock
    Signed-off-by: Peter <PeterRong96@gmail.com>

--- src/interface.rs
+++ src/interface.rs
@@ -5,13 +5,3 @@ pub mod console {
         fn write_fmt(&self, args: fmt::Arguments) -> fmt::Result;
     }
 }
-
-pub mod sync {
-    pub trait Mutex {
-        type Data;
-
-        fn lock<R, F>(&mut self, f: F) -> R
-        where
-            F: FnOnce(&mut Self::Data) -> R;
-    }
-}
